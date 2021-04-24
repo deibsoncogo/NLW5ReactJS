b9dab92207c76da2f42e402faf23280dd74de934
Deibson Cogo <deibsoncogo@outlook.com>
2021-04-24 14:49:16 -0300

    Criação de uma saudação do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import format from "date-fns/format";
 import ptBR from "date-fns/locale/pt-BR";
 import style from "./style.module.scss";
@@ -5,11 +6,25 @@ import style from "./style.module.scss";
 export default function Header() {
   const currentDate = format(new Date(), "EEEEEE, d MMMM", { locale: ptBR });
 
+  const [salutation, setSalutation] = useState("");
+
+  const hora = Number(format(new Date(), "HH", { locale: ptBR }));
+
+  useEffect(() => {
+    if (hora >= 0 && hora < 12) {
+      setSalutation("Bom dia");
+    } else if (hora >= 12 && hora < 18) {
+      setSalutation("Bom tarde");
+    } else if (hora >= 18 && hora < 24) {
+      setSalutation("Bom noite");
+    }
+  }, [hora]);
+
   return (
     <header className={style.container}>
       <img src="/logo.svg" alt="Podcastr" />
 
-      <p>O melhor para você ouvir, sempre</p>
+      <p>{salutation}, o melhor para você sempre ouvir!</p>
 
       <span>{currentDate}</span>
     </header>
