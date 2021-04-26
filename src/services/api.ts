19eb5299724771d9a2873068443d0e84de4d79e0
Deibson Cogo <deibsoncogo@outlook.com>
2021-04-26 10:08:41 -0300

    Novos ajustes para o My JSON Server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
 
 // eslint-disable-next-line import/prefer-default-export
-export const api = axios.create({ baseURL: process.env.API_URL || "http://localhost:3333/" });
+export const api = axios.create({
+  baseURL: process.env.NODE_ENV === "development"
+    ? "http://localhost:3333/"
+    : "https://my-json-server.typicode.com/deibsoncogo/NLW5ReactJS",
+});
